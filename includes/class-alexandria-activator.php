2f37d5ab08d63721605777c5d0288cf450be2ba8
MARIE David <eth3rnit3@MacBook-Pro-de-MARIE-2.local>
2020-07-21 14:46:20 +0200

    init plugin

---
+++ includes/class-alexandria-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://dmkreation.com
+ * @since      1.0.0
+ *
+ * @package    Alexandria
+ * @subpackage Alexandria/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Alexandria
+ * @subpackage Alexandria/includes
+ * @author     David <eth3rnit3@gmail.com>
+ */
+class Alexandria_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
